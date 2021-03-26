35b20af25f1f33bff61dc43bf10e405ceea901d8
eoriont <elijahotarr@gmail.com>
2021-03-25 20:52:48 -0700

    adding epic file that is super useful

---
+++ elijah_comment.ex
@@ -0,0 +1,23 @@
+defmodule Elijah do
+  @moduledoc """
+  This module is full of useful helper functions for Elijah.
+  """
+
+  @moduledoc since: "1.0.0"
+
+  @doc """
+  Returns the name of the person who this module is for.
+
+  Returns `:string`
+
+  ## Examples
+
+    iex> Elijah.what_is_his_name?
+    "His name is Elijah"
+
+  """
+  @doc since: "1.0.0"
+  def what_is_his_name? do
+    "His name is Elijah"
+  end
+end
