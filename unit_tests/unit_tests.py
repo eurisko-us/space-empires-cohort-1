8f33ef40a74ad523e7c9f75f215bd55fe8f04e2c
Colby Roberts <colbiverse@gmail.com>
2021-04-26 22:16:04 -0700

    Assignment 117 Completed

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -1,17 +1,17 @@
 import sys
 sys.path.append("src")
-print(sys.path)
 from economic_engine import EconomicEngine
 from combat_engine import CombatEngine
 from movement_engine import MovementEngine
 from state import State
-sys.path.pop(-1)
+
+del sys.path[-1]
 
 import os
 rootdir = 'unit_tests'
 
+#get phases
 phases = {}
-
 for subdir, dirs, files in os.walk(rootdir):
     for folder in dirs:
         if folder != '__pycache__':
@@ -20,31 +20,32 @@ for subdir, dirs, files in os.walk(rootdir):
                 phases[phase[0]] = []
             phases[phase[0]].append(int(phase[2]))
 
-
-
+#run all phases
 for phase, test_indexs in phases.items():
     for test_index in test_indexs:
-        file_path = 'unit_tests\\' + phase + str('_test_') + str(test_index)
-        sys.path.append(file_path)
-        from initial_state import game_state as initial_state
-        from final_state import game_state as testing_final_state
-        from strategies import Strategy
-        strats = [Strategy, Strategy]
+        file_path = phase + str('_test_') + str(test_index)
+        run_test(file_path)
 
-        state = State.from_standard(initial_state, strats)
-        if phase == 'economic':
-            EconomicEngine.run_phase(state)
+#run specific phase
+def run_test(folder_name):
+    sys.path.append('unit_tests/'+folder_name)
+    from initial_state import game_state as initial_state
+    from final_state import game_state as testing_final_state
+    from strategies import Strategy
+    strats = [Strategy, Strategy]
 
-        if phase == 'combat':
-            CombatEngine.run_phase(state)
+    state = State.from_standard(initial_state, strats)
+    if phase == 'economic':
+        EconomicEngine.run_phase(state)
 
-        if phase == 'movement':
-            MovementEngine.run_phase(state)
+    if phase == 'combat':
+        CombatEngine.run_phase(state)
 
-        final_state = State.from_standard(testing_final_state, strats)
+    if phase == 'movement':
+        MovementEngine.run_phase(state)
 
-        assert State.compare_native_states(final_state, testing_final_state), "Tests failed" #his native states doesnt work so i cant see if my method works yet
+    final_state = State.from_standard(testing_final_state, strats)
 
-        print(phase + str('_test_') + str(test_index) + ' Passed!!') 
-        sys.path.pop(-1)
-
+    assert State.compare_native_states(final_state, testing_final_state), phase + str('_test_') + str(test_index) + ' failed...'
+    print(phase + str('_test_') + str(test_index) + ' Passed!!') 
+    sys.path.pop(-1)
